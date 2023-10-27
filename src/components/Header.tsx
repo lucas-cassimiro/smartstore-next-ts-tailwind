1b26e43c989afd5ba5666592df2f5a72647e1e58
Lucas Cassimiro <lucascassimiro545@hotmail.com>
2023-10-26 22:10:36 -0300

    layout e header em andamento, adicionado NavLinks e rotas sendo em conclusão

---
+++ src/components/Header.tsx
@@ -0,0 +1,55 @@
+import { NavLink } from "@/app/layout";
+import Navigation from "./Client-components/Navigation";
+import Link from "next/link";
+
+import Image from "next/image";
+
+import { Lobster } from "next/font/google";
+
+interface NavLinkProps {
+  navLinks: NavLink[];
+}
+
+const lobster = Lobster({
+  subsets: ["latin"],
+  weight: "400",
+  display: "swap",
+  style: "normal",
+});
+
+interface NavLinkProps {
+  navLinks: NavLink[];
+}
+
+export default function Header({ navLinks }: NavLinkProps) {
+  return (
+    <header>
+      <div>
+        <p>
+          A melhor franquia de smartphones do
+          <span>Brasil</span>
+        </p>
+
+        <a href="#">SEJA UM REVENDEDOR</a>
+      </div>
+
+      <div>
+        <h1>
+          <Link href="/" style={lobster.style} className={lobster.className}>
+            Smart Store
+          </Link>
+        </h1>
+
+        <Link href="/carrinho">
+          <div>
+            {/* <Image src={IconCart} alt="Ícone de carrinho" /> */}
+          </div>
+        </Link>
+      </div>
+
+      <nav>
+        <Navigation navLinks={navLinks} />
+      </nav>
+    </header>
+  );
+}
