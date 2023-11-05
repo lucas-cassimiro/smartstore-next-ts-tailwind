f4f1f62be2172e04b7233bf33fa170cf582ecf69
Lucas Cassimiro <lucascassimiro545@hotmail.com>
2023-11-05 19:16:24 -0300

    types input edited

--- src/components/RegisterFrame.tsx
+++ src/components/RegisterFrame.tsx
@@ -8,21 +8,21 @@ export default function RegisterFrame() {
           Criar uma conta
         </h3>
         <span>Email</span>
-        <input type="text" className="bg-[#ecf2f7]" />
+        <input type="email" className="bg-[#ecf2f7]" />
         <span>Senha</span>
         <input type="password" className="bg-[#ecf2f7]" />
         <span>Confirmar senha</span>
         <input type="password" className="bg-[#ecf2f7]" />
         <span>CPF</span>
-        <input type="password" className="bg-[#ecf2f7]" />
+        <input type="tel" className="bg-[#ecf2f7]" />
         <span>Nome</span>
         <input type="text" className="bg-[#ecf2f7]" />
         <span>Sobrenome</span>
         <input type="text" className="bg-[#ecf2f7]" />
         <span>Data de nascimento</span>
-        <input type="password" className="bg-[#ecf2f7]" />
+        <input type="date" className="bg-[#ecf2f7]" />
         <span>Celular</span>
-        <input type="password" className="bg-[#ecf2f7]" />
+        <input type="tel" className="bg-[#ecf2f7]" />
         <button className="bg-[#4aa4ee] text-white font-light cursor-pointer rounded-md hover:bg-[#3286ca] h-[50px] text-lg">
           Continuar
         </button>
