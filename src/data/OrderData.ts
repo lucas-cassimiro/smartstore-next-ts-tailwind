eebb3b83820a0cce8cb020e297de57e8d54e6f17
Lucas Cassimiro <lucascassimiro545@hotmail.com>
2023-11-14 16:52:41 -0300

    orders data criado

---
+++ src/data/OrderData.ts
@@ -0,0 +1,8 @@
+import React from "react";
+
+export const orders = [
+  { label: "Selecione uma opção", value: "" },
+  { label: "Mais procurados", value: "procurados" },
+  { label: "Mais recentes", value: "recentes" },
+  { label: "Mais vendidos", value: "vendidos" },
+];
