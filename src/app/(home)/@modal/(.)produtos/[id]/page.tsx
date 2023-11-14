81b52e749ef7fda2d132496166de92fafb1c2222
Lucas Cassimiro <lucascassimiro545@hotmail.com>
2023-11-14 16:45:17 -0300

    Modal de produtos criado página de detalhes

---
+++ src/app/(home)/@modal/(.)produtos/[id]/page.tsx
@@ -0,0 +1,24 @@
+import ProductFrame from "@/components/ProductFrame";
+import { ProductsData } from "@/interfaces/ProductsData";
+
+import Modal from "@/components/Modal";
+
+async function getProductById(productId: number): Promise<ProductsData[]> {
+  const request = await fetch(`http://localhost:3001/produto/${productId}`);
+  const data = await request.json();
+  return data;
+}
+
+export default async function ProductModal({
+  params,
+}: {
+  params: { id: number };
+}) {
+  const product = await getProductById(params.id);
+
+  return (
+    <Modal>
+      <ProductFrame products={product} />
+    </Modal>
+  );
+}
