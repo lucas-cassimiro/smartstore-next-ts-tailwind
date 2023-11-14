b165b17c5868f8e8402412af9e789587783e1641
Lucas Cassimiro <lucascassimiro545@hotmail.com>
2023-11-14 16:45:26 -0300

    ModalRegister

---
+++ src/app/(home)/@modal/(.)register/page.tsx
@@ -0,0 +1,11 @@
+import LoginFrame from "@/components/LoginFrame";
+import Modal from "@/components/Modal";
+import RegisterFrame from "@/components/RegisterFrame";
+
+export default function ModalRegister() {
+  return (
+    <Modal>
+      <RegisterFrame />
+    </Modal>
+  );
+}
