0e1478a7760b6ceb551fc1ee5c419bb8b4ff8635
Lucas Cassimiro <lucascassimiro545@hotmail.com>
2023-11-14 16:44:55 -0300

    ModalPassword

---
+++ src/app/(home)/@modal/(.)password/page.tsx
@@ -0,0 +1,10 @@
+import Modal from "@/components/Modal";
+import PasswordFrame from "@/components/PasswordFrame";
+
+export default function ModalPassword() {
+  return (
+    <Modal>
+      <PasswordFrame />
+    </Modal>
+  );
+}
