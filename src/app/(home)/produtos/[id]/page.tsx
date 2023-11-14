3b69fe19cc0b0759f0d9d069aa7a265a1e752232
Lucas Cassimiro <lucascassimiro545@hotmail.com>
2023-11-14 16:45:47 -0300

    ProductPage criada

---
+++ src/app/(home)/produtos/[id]/page.tsx
@@ -0,0 +1,18 @@
+import { ProductsData } from "@/interfaces/ProductsData";
+
+import ProductFrame from "@/components/ProductFrame";
+
+async function getProductById(productId: number): Promise<ProductsData[]> {
+  const request = await fetch(`http://localhost:3001/produto/${productId}`);
+  const data = await request.json();
+  return data;
+}
+
+export default async function ProductPage({
+  params,
+}: {
+  params: { id: number };
+}) {
+  const product = await getProductById(params.id);
+  return <ProductFrame products={product} />;
+}
