7690c77774b725a3bdc43080822455290cc26855
Lucas Cassimiro <lucascassimiro545@hotmail.com>
2023-11-14 16:44:48 -0300

    ModalLogin

---
+++ src/app/(home)/@modal/(.)login/page.tsx
@@ -0,0 +1,10 @@
+import LoginFrame from "@/components/LoginFrame";
+import Modal from "@/components/Modal";
+
+export default function ModalLogin() {
+  return (
+    <Modal>
+      <LoginFrame />
+    </Modal>
+  );
+}
