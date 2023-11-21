da02472ed927b13d622184eb6dcbf86c6c50e6d2
Lucas Cassimiro <lucascassimiro545@hotmail.com>
2023-11-21 16:06:48 -0300

    modal isauthenticated

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -3,11 +3,15 @@
 import { useRef, useCallback } from "react";
 import { useRouter } from "next/navigation";
 
+import { useAuth } from "@/hooks/useAuth";
+
 interface ModalProps {
   children: React.ReactNode;
 }
 
 export default function Modal({ children }: ModalProps) {
+  const { isAuthenticated } = useAuth();
+
   const overlay = useRef<HTMLDivElement>(null);
   const wrapper = useRef<HTMLDivElement>(null);
 
@@ -27,17 +31,23 @@ export default function Modal({ children }: ModalProps) {
   );
 
   return (
-    <div
-      ref={overlay}
-      className="fixed z-10 left-0 right-0 top-0 bottom-0 mx-auto bg-black/40 scrollbar overflow-y-auto"
-      onClick={onClick}
-    >
-      <div
-        ref={wrapper}
-        className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-1/2 tablet:w-10/12 p-6"
-      >
-        {children}
-      </div>
-    </div>
+    <>
+      {!isAuthenticated ? (
+        <div
+          ref={overlay}
+          className="fixed z-10 left-0 right-0 top-0 bottom-0 mx-auto bg-black/40 scrollbar overflow-y-auto"
+          onClick={onClick}
+        >
+          <div
+            ref={wrapper}
+            className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-1/2 tablet:w-10/12 p-6"
+          >
+            {children}
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
+    </>
   );
 }
