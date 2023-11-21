8d0c960bce8926bfb8f635d70df1536410239c25
Lucas Cassimiro <lucascassimiro545@hotmail.com>
2023-11-21 16:06:10 -0300

    useauth implementado

--- src/app/(home)/@modal/(.)login/page.tsx
+++ src/app/(home)/@modal/(.)login/page.tsx
@@ -1,10 +1,15 @@
+"use client";
+
 import LoginFrame from "@/components/LoginFrame";
 import Modal from "@/components/Modal";
+import { useAuth } from "@/hooks/useAuth";
 
 export default function ModalLogin() {
+
   return (
     <Modal>
       <LoginFrame />
     </Modal>
   );
+  
 }
