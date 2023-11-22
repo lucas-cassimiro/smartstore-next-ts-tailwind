7707da93b3ba6abcfdb556b90d02cb33f2db1243
Lucas Cassimiro <lucascassimiro545@hotmail.com>
2023-11-21 22:54:04 -0300

    imports commented

--- src/components/LoginFrame.tsx
+++ src/components/LoginFrame.tsx
@@ -1,13 +1,17 @@
 "use client";
 
-import Link from "next/link";
+//import Link from "next/link";
 import Image from "next/image";
+
+import React from "react";
 
 import Google from "../assets/google.png";
 import Facebook from "../assets/facebook.png";
 import Apple from "../assets/apple.png";
 
-import { Button, Checkbox, Input, Switch } from "@nextui-org/react";
+//import { Button, Checkbox, Input, Switch } from "@nextui-org/react";
+
+import { FaUserAlt } from "react-icons/fa";
 
 import { useForm, SubmitHandler, SubmitErrorHandler } from "react-hook-form";
 
@@ -22,6 +26,21 @@ import { NavLink } from "@/Providers/LayoutProvider";
 import { useAuth } from "@/hooks/useAuth";
 import { AuthContext } from "@/contexts/AuthContext";
 
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Button,
+  useDisclosure,
+  Checkbox,
+  Input,
+  Link,
+} from "@nextui-org/react";
+import { MailIcon } from "./MailIcon";
+import { LockIcon } from "./LockIcon";
+
 export type FormData = {
   email: string;
   password_hash: string;
@@ -33,6 +52,8 @@ export type FormData = {
 
 export default function LoginFrame() {
   const { signIn, errorMessage } = useContext(AuthContext);
+
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
 
   const {
     handleSubmit,
@@ -57,93 +78,122 @@ export default function LoginFrame() {
   const onError: SubmitErrorHandler<FormData> = (errors) => console.log(errors);
 
   return (
-    <div className="w-full h-full flex justify-center items-center">
-      <div className="flex flex-col bg-white h-[550px] w-[600px] justify-center p-5 rounded-md gap-4">
-        <h3 className="text-center uppercase text-2xl font-semibold">Login</h3>
-        <form onSubmit={handleSubmit(handleSignIn, onError)} className="flex flex-col gap-3">
-          <Input
-            type="text"
-            isRequired
-            label="Email"
-            {...register("email", {
-              required: "O endereço de e-mail é obrigatório.",
-              pattern: {
-                value:
-                  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-                message:
-                  "Entrada inválida. Informe um endereço de e-mail válido. Por exemplo, lucas@cassimiro.com.",
-              },
-            })}
-            // onClick={() => {
-            //   if (getValues("email").trim() && errorMessage) {
-            //     setErrorMessage(null);
-            //   }
-            // }}
-          />
-          {errors?.email && (
-            <span className="text-[#a94442] text-sm">
-              {errors?.email.message}
-            </span>
-          )}
-          <Input
-            type="password"
-            label="Senha"
-            isRequired
-            minLength={6}
-            maxLength={255}
-            autoComplete="given-password"
-            {...register("password_hash", {
-              required: "A senha é obrigatória.",
-            })}
-            // onInput={() => {
-            //   if (errorMessage?.message) {
-            //     setErrorMessage(null);
-            //   }
-            // }}
-          />
-          {errors?.password_hash && (
-            <span className="text-[#a94442] text-sm">
-              {errors?.password_hash.message}
-            </span>
-          )}
-          {errorMessage && (
-            <span className="text-[#a94442] text-sm">{errorMessage}</span>
-          )}
+    <>
+      <Button
+        onPress={onOpen}
+        color="primary"
+        className="relative flex items-center"
+      >
+        <div className="flex flex-col items-center">
+          <FaUserAlt className="text-white celular:w-4 celular:h-4 w-6 h-5" />
+          <span className="text-white celular:text-sm text-sm">Login</span>
+        </div>
+      </Button>
+      <Modal
+        isOpen={isOpen}
+        onOpenChange={onOpenChange}
+        placement="top-center"
+        backdrop="blur"
+      >
+        <ModalContent style={{ backgroundColor: "hsl(240, 6%, 10%)" }}>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1 text-white">ENTRAR</ModalHeader>
+              <ModalBody>
+                <form
+                  onSubmit={handleSubmit(handleSignIn, onError)}
+                  className="flex flex-col gap-3"
+                >
+                  <Input
+                    autoFocus
+                    endContent={
+                      <MailIcon className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
+                    }
+                    isRequired
+                    label="Email"
+                    placeholder="Digite seu e-mail"
+                    variant="bordered"
+                    className="bg-hsl(240 6% 10%) "
+                    {...register("email", {
+                      required: "O endereço de e-mail é obrigatório.",
+                      pattern: {
+                        value:
+                          /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                        message:
+                          "Entrada inválida. Informe um endereço de e-mail válido. Por exemplo, lucas@cassimiro.com.",
+                      },
+                    })}
+                    // onClick={() => {
+                    //   if (getValues("email").trim() && errorMessage) {
+                    //     setErrorMessage(null);
+                    //   }
+                    // }}
+                  />
+                  {errors?.email && (
+                    <span className="text-[#a94442] text-sm">
+                      {errors?.email.message}
+                    </span>
+                  )}
 
-          <div>
-            <div className="flex justify-between items-center">
-              <Switch defaultSelected>Lembrar-me</Switch>
-              <Link
-                href="/password"
-                className="text-[#4aa4ee] hover:text-[#3286ca]"
-              >
-                Esqueceu a senha?
-              </Link>
-            </div>
-          </div>
-          {/* <button className="bg-[#4aa4ee] text-white font-light cursor-pointer rounded-md hover:bg-[#3286ca] h-[50px] text-lg transition-all duration-700 ease-in-out">
-            Entrar
-          </button> */}
-          <Button type="submit" color="primary" className="w-full text-base">Entrar</Button>
-        </form>
-        <div className="flex gap-1">
-          <span>Não possui uma conta?</span>
-          <Link
-            href="/register"
-            className="text-[#4aa4ee] hover:text-[#3286ca]"
-          >
-            Inscrever-se
-          </Link>
-        </div>
-        <h4 className="text-center text-[#607d8b] font-medium text-[22px]">
-          Logar com
-        </h4>
-        <div className="flex justify-center gap-2 items-center">
-          <Image src={Facebook} alt="Ícone do Facebook" className="w-10 h-10" />
-          <Image src={Google} alt="Ícone do Google" className="w-10 h-10" />
-          <Image src={Apple} alt="Ícone da Apple" className="w-10 h-10" />
-        </div>
-      </div>
-    </div>
+                  <Input
+                    endContent={
+                      <LockIcon className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
+                    }
+                    isRequired
+                    label="Senha"
+                    placeholder="Digite sua senha"
+                    type="password"
+                    variant="bordered"
+                    minLength={6}
+                    maxLength={255}
+                    autoComplete="given-password"
+                    {...register("password_hash", {
+                      required: "A senha é obrigatória.",
+                    })}
+                    // onInput={() => {
+                    //   if (errorMessage?.message) {
+                    //     setErrorMessage(null);
+                    //   }
+                    // }}
+                  />
+                  {errors?.password_hash && (
+                    <span className="text-[#a94442] text-sm">
+                      {errors?.password_hash.message}
+                    </span>
+                  )}
+                  {errorMessage && (
+                    <span className="text-[#a94442] text-sm">
+                      {errorMessage}
+                    </span>
+                  )}
+
+                  <div className="flex py-2 px-1 justify-between">
+                    <Checkbox
+                      classNames={{
+                        label: "text-small",
+                      }}
+                     
+                    >
+                      Lembrar-me
+                    </Checkbox>
+                    <Link color="primary" href="#" size="sm">
+                      Esqueceu sua senha?
+                    </Link>
+                  </div>
+                  <ModalFooter className="px-0">
+                    <Button color="danger" variant="flat" onPress={onClose}>
+                      Fechar
+                    </Button>
+                    <Button type="submit" color="primary">
+                      Entrar
+                    </Button>
+                  </ModalFooter>
+                </form>
+              </ModalBody>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+    </>
   );
 }
