2647b7c843ac2dbfcdf7ae8f6a46f02f4cc1ef43
Lucas Cassimiro <lucascassimiro545@hotmail.com>
2023-11-25 18:11:17 -0300

    camel case

--- src/components/UserLayout.tsx
+++ src/components/UserLayout.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { NavLink } from "../Providers/LayoutProvider";
+import { NavLink } from "../providers/LayoutProvider";
 import Link from "next/link";
 
 import { usePathname } from "next/navigation";
