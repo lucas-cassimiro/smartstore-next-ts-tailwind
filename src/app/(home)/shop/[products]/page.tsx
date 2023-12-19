3cc3ab78b37929c96df5da47cb134e9244effe47
Lucas Cassimiro <lucascassimiro545@hotmail.com>
2023-12-19 13:09:12 -0300

    localhost api

--- src/app/(home)/shop/[products]/page.tsx
+++ src/app/(home)/shop/[products]/page.tsx
@@ -9,12 +9,10 @@ import { Select, SelectItem } from "@nextui-org/react";
 import { ProductsData } from "@/interfaces/ProductsData";
 import { useState } from "react";
 import ButtonFilterMobile from "@/components/Client-components/ButtonFilterMobile";
+import SelectCategorie from "@/components/Client-components/ButtonFilter/SelectCategorie";
 
 async function getProducts(param: string): Promise<ProductsData[]> {
-  const response = await fetch(
-    `https://smartshop-api-foy4.onrender.com/produto/${param}`
-  );
-
+  const response = await fetch(`http://localhost:3333/products/${param}`);
   const data = await response.json();
   return data;
 }
@@ -33,7 +31,11 @@ export default async function Products({
       <div className="p-10">
         <p className="w-full">
           <strong>Você está em: </strong>
-          {categorie}
+          {categorie === "iphone" && <span>iPhones</span>}
+          {categorie === "android" && <span>Androids</span>}
+          {categorie === "smartwatch" && <span>Smartwatchs</span>}
+          {categorie === "fone" && <span>Fone Bluetooth</span>}
+          {categorie === "blackfriday" && <span>Black Friday</span>}
         </p>
         <div className="flex  ">
           <div className="mt-14 min-w-[13.9375rem] tabletgrande:hidden">
@@ -51,32 +53,17 @@ export default async function Products({
             <ButtonFilter categorie={categorie} />
           </div>
 
-          <div className="flex flex-col max-w-full min-w-[500px]  ml-14 tabletgrande:ml-0">
+          <div className="flex flex-col w-full ml-14 tabletgrande:ml-0">
             <ButtonFilterMobile />
 
             <div className="h-[2rem] flex items-center gap-3 w-full justify-end mb-20 tabletgrande:hidden">
-              <label htmlFor="ordenar" className="text-base font-semibold">
-                Ordenar por:
-              </label>
-              <select
-                name="ordenar"
-                id="ordenar"
-                //   onChange={(e) => setOrder(e.target.value)}
-                className="w-[15.25rem] h-[2.375rem] cursor-pointer text-base font-normal text-[#313131] py-0 px-[0.625rem] border-[rgba(158, 158, 158, 0.87)] border-2 rounded-lg"
-              >
-                <option value="">Selecione uma categoria</option>
-                <option value="procurados">Mais procurados</option>
-                <option value="recentes">Mais recentes</option>
-                <option value="vendidos">Mais vendidos</option>
-              </select>
+              <SelectCategorie />
             </div>
+
             <div className="flex flex-wrap">
-              {data.length >= 1 ? (
-                <CardProducts products={data} />
-              ) : (
-                <span>Não há produtos para exibição.</span>
-              )}
-              {/*<CardProducts products={data} />*/}
+              {data.map((data) => (
+                <CardProducts product={data} key={data.id} />
+              ))}
             </div>
           </div>
         </div>
