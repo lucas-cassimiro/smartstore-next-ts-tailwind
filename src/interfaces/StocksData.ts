453b87f1c3192cc5515f1efbbfb11eec3ec4b4ed
Lucas Cassimiro <lucascassimiro545@hotmail.com>
2023-12-19 13:13:26 -0300

    stocks data interface

---
+++ src/interfaces/StocksData.ts
@@ -0,0 +1,10 @@
+export interface StocksData {
+  id: number;
+  product_id: number;
+  status: string;
+  purchase_price: number;
+  expiry_date: Date;
+  created_at: Date;
+  updated_at: Date;
+  quantity: number;
+}
