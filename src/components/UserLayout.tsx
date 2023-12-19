918bb7c4454da5d167f0a65db981b003d5b82e9a
Lucas Cassimiro <lucascassimiro545@hotmail.com>
2023-12-19 13:11:57 -0300

    breadcrumbs

--- src/components/UserLayout.tsx
+++ src/components/UserLayout.tsx
@@ -7,39 +7,52 @@ import { usePathname } from "next/navigation";
 
 import { GrFormNext } from "react-icons/gr";
 
+import { Breadcrumbs, BreadcrumbItem } from "@nextui-org/breadcrumbs";
+
 interface NavLinkProps {
   navLinks: NavLink[];
 }
 
 export default function UserLayout({ navLinks }: NavLinkProps) {
+  const menuNavLinks: NavLink[] = [
+    {
+      name: "Meu cadastro",
+      href: "/meu-cadastro",
+    },
+    {
+      name: "Meus pedidos",
+      href: "/orderHistory",
+    },
+  ];
+
   const pathname = usePathname();
 
   return (
-    <section className="">
-      <div className="flex gap-2 mb-8 w-full text-[13px]">
-        <Link href="/" className="text-[#333]">
-          Home /
-        </Link>
-        {navLinks.map((link) => {
-          const isActive = pathname.startsWith(link.href);
-          return (
-            <li
-              key={link.name}
-              className={`${isActive ? "block" : "hidden"} list-none`}
-            >
-              <Link href={link.href} className="text-[#333333] text-[13px]">
-                {" "}
+    <section>
+      <div className="flex flex-col flex-wrap gap-4 mb-5">
+        <Breadcrumbs variant="solid">
+          <BreadcrumbItem href="/">Home</BreadcrumbItem>
+          {navLinks.map((link) => {
+            const isActive = pathname.startsWith(link.href);
+            return (
+              <BreadcrumbItem
+                href={link.href}
+                key={link.name}
+                className={`${
+                  isActive ? "flex" : "hidden"
+                } tablet:hidden list-none`}
+              >
                 {link.name}
-              </Link>
-            </li>
-          );
-        })}
+              </BreadcrumbItem>
+            );
+          })}
+        </Breadcrumbs>
       </div>
       <div className="flex flex-col gap-6 w-[300px]">
         <div className="border-b-[1px] border-black flex">
           <span className="font-semibold text-lg mb-5">Minha conta</span>
         </div>
-        {navLinks.map((link) => {
+        {menuNavLinks.map((link) => {
           const isActive = pathname.endsWith(link.href);
           return (
             <li
