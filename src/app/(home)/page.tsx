2bbca525fe3017a68a4b9a8ec4869759d5f9c3f4
Lucas Cassimiro <lucascassimiro545@hotmail.com>
2023-12-19 13:07:59 -0300

    removed newsData

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -6,10 +6,9 @@ import localFont from "next/font/local";
 import Blackfriday from "../../assets/banner-blackfriday.png";
 import Whatsapp from "../../assets/banner-whatsapp.png";
 import NewsCarousel from "@/lib/NewsCarousel";
+import BlackFridayCarousel from "@/lib/BlackFridayCarousel";
 
 const jejugothic = localFont({ src: "../../fonts/jejugothic-regular.ttf" });
-
-import { news } from "@/data/NewsProducts";
 
 export default function Home() {
   return (
@@ -22,17 +21,29 @@ export default function Home() {
         >
           Novidades
         </span>
-        <NewsCarousel products={news} />
+        <NewsCarousel />
         <span
           className="flex justify-center text-[2.1875rem] font-normal my-4 tablet:text-[1.675rem] celular:text-[1.2rem]"
           style={jejugothic.style}
         >
           Black Friday
         </span>
-        <div className="flex flex-col gap-8 justify-center w-full items-center">
-          <Image src={Blackfriday} alt="Banner Black Friday" priority={true} />
-          <Image src={Whatsapp} alt="Banner WhatsApp" priority={true} />
-        </div>
+
+        <Image
+          src={Blackfriday}
+          alt="Banner Black Friday"
+          priority={true}
+          className="mx-auto"
+        />
+
+        <BlackFridayCarousel />
+
+        <Image
+          src={Whatsapp}
+          alt="Banner WhatsApp"
+          priority={true}
+          className="mx-auto mb-[2.8125rem]"
+        />
       </div>
     </>
   );
