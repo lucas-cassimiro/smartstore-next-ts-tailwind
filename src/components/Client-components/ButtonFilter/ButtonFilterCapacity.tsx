a1f052e5c6247ffbf3eaa6795283da80b46f9c24
Lucas Cassimiro <lucascassimiro545@hotmail.com>
2023-12-19 13:12:30 -0300

    button filter capacity add

--- src/components/Client-components/ButtonFilter/ButtonFilterCapacity.tsx
+++ src/components/Client-components/ButtonFilter/ButtonFilterCapacity.tsx
@@ -1,96 +1,34 @@
 import { Checkbox } from "@nextui-org/react";
 
-import { ProductsData } from "@/interfaces/ProductsData";
-
-interface ButtonFilterCapacityProps {
-  categorie: string;
-}
-
-export default function ButtonFilterCapacity({
-  categorie,
-}: ButtonFilterCapacityProps) {
+export default function ButtonFilterCapacity() {
   return (
     <>
-      {categorie === "fone" || categorie === "smartwatch" ? (
-        <>
-          <label className="cursor-pointer">
-            <Checkbox
-              type="checkbox"
-              name="armazenamento"
-              value="64"
-              disabled
-            />
-            64 GB
-          </label>
-          <label className="cursor-pointer">
-            <Checkbox
-              type="checkbox"
-              name="armazenamento"
-              value="128"
-              disabled
-            />
-            128 GB
-          </label>
-          <label className="cursor-pointer">
-            <Checkbox
-              type="checkbox"
-              name="armazenamento"
-              value="256"
-              disabled
-            />
-            256 GB
-          </label>
-          <label className="cursor-pointer">
-            <Checkbox
-              type="checkbox"
-              name="armazenamento"
-              value="512"
-              disabled
-            />
-            512 GB
-          </label>
-          <label className="cursor-pointer">
-            <Checkbox
-              type="checkbox"
-              name="armazenamento"
-              value="1024"
-              //  onChange={handleChangeStorage}
-              className="mb-3"
-              disabled
-            />
-            1 TB
-          </label>
-        </>
-      ) : (
-        <>
-          <label className="cursor-pointer">
-            <Checkbox type="checkbox" name="armazenamento" value="64" />
-            64 GB
-          </label>
-          <label className="cursor-pointer">
-            <Checkbox type="checkbox" name="armazenamento" value="128" />
-            128 GB
-          </label>
-          <label className="cursor-pointer">
-            <Checkbox type="checkbox" name="armazenamento" value="256" />
-            256 GB
-          </label>
-          <label className="cursor-pointer">
-            <Checkbox type="checkbox" name="armazenamento" value="512" />
-            512 GB
-          </label>
-          <label className="cursor-pointer">
-            <Checkbox
-              type="checkbox"
-              name="armazenamento"
-              value="1024"
-              //  onChange={handleChangeStorage}
-              className="mb-3"
-            />
-            1 TB
-          </label>{" "}
-        </>
-      )}
+      <label className="cursor-pointer">
+        <Checkbox type="checkbox" name="armazenamento" value="64" />
+        64 GB
+      </label>
+      <label className="cursor-pointer">
+        <Checkbox type="checkbox" name="armazenamento" value="128" />
+        128 GB
+      </label>
+      <label className="cursor-pointer">
+        <Checkbox type="checkbox" name="armazenamento" value="256" />
+        256 GB
+      </label>
+      <label className="cursor-pointer">
+        <Checkbox type="checkbox" name="armazenamento" value="512" />
+        512 GB
+      </label>
+      <label className="cursor-pointer">
+        <Checkbox
+          type="checkbox"
+          name="armazenamento"
+          value="1024"
+          //  onChange={handleChangeStorage}
+          className="mb-3"
+        />
+        1 TB
+      </label>
     </>
   );
 }
