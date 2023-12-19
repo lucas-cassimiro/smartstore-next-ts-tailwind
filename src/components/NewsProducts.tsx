dbecae8670402d5dd3b408b0a6ba5446ba679aa1
Lucas Cassimiro <lucascassimiro545@hotmail.com>
2023-12-19 13:11:38 -0300

    link next add

--- src/components/NewsProducts.tsx
+++ src/components/NewsProducts.tsx
@@ -7,6 +7,8 @@ import { news } from "@/data/NewsProducts";
 
 import currencyFormat from "@/helpers/currencyFormat";
 
+import Link from "next/link";
+
 const inter = Inter({ subsets: ["latin"] });
 
 const jejugothic = localFont({
@@ -22,26 +24,37 @@ const jejugothic = localFont({
 export default function NewsProducts({ product }: any) {
   return (
     <>
-      <div
-        style={jejugothic.style}
-        className="flex flex-col items-center text-white h-[28.125rem] cursor-pointer  bg-black"
+      <Link
+        href={`/produtos/${product.id}`}
+        key={product.id}
+        className="flex flex-col flex-2 m-[1rem] "
       >
-        <div className="flex items-center justify-center w-[25rem] h-[21.875rem]">
-          <Image
-            className="w-[17.5rem] h-[15.625rem] object-contain"
-            src={product.img}
-            alt="aaaa"
-          />
-          {/* <span className="text-black">Imagem do produto</span> */}
+        <div
+          style={jejugothic.style}
+          className="flex flex-col items-center text-white h-[28.125rem] cursor-pointer bg-black"
+        >
+          <div className="flex items-center justify-center w-[25rem] h-[21.875rem]">
+            <Image
+              className="w-[16.5rem] h-[14.625rem] object-contain"
+              src={`http://localhost:3333/tmp/uploads/${product.image}`}
+              alt="Imagem do produto"
+              quality={80}
+              priority={true}
+              width={350}
+              height={350}
+            />
+          </div>
+          <h2 className="text-[1.875rem] font-normal text-white">
+            {product.name}
+          </h2>
+          <p className={`${inter.className} text-xl font-light text-white`}>
+            A partir de{" "}
+            <span className="text-[#93c1fd]">
+              {currencyFormat(product.price)}
+            </span>
+          </p>
         </div>
-        <h2 className="text-[1.875rem] font-normal text-white">
-          {product.name}
-        </h2>
-        <p className={`${inter.className} text-xl font-light text-white`}>
-          A partir de{" "}
-          <span className="text-[#93c1fd]">{currencyFormat(product.price)}</span>
-        </p>
-      </div>
+      </Link>
     </>
   );
 }
