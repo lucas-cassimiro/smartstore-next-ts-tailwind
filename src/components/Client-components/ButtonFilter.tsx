4ede272af6355e8839088f3ba7a8b1b3ce8d3445
Lucas Cassimiro <lucascassimiro545@hotmail.com>
2023-12-19 13:12:16 -0300

    button filter validation

--- src/components/Client-components/ButtonFilter.tsx
+++ src/components/Client-components/ButtonFilter.tsx
@@ -15,8 +15,13 @@ export default function ButtonFilter({ categorie }: ButtonFilterProps) {
     <CheckboxGroup className="flex flex-col w-[12rem]" color="primary">
       <h4 className="font-bold mb-3">Preço</h4>
       <ButtonFilterPrice />
-      <h4 className="font-bold mb-3">Armazenamento</h4>
-      <ButtonFilterCapacity categorie={categorie} />
+      {categorie !== "smartwatch" && categorie !== "fone" && (
+        <>
+          <h4 className="font-bold mb-3">Armazenamento</h4>
+          <ButtonFilterCapacity />
+        </>
+      )}
+
       <h4 className="font-bold mb-3">Cor</h4>
       <ButtonFilterColor />
     </CheckboxGroup>
