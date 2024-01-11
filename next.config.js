288fce499dcfdaff55e960cd5d0a7a8752eeb8ff
Lucas Cassimiro <lucascassimiro545@hotmail.com>
2024-01-11 13:16:53 -0300

    domain smartshop render and localhost

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["localhost"],
+    domains: ["localhost", "smartshop-api-foy4.onrender.com"],
   },
 };
 
-module.exports = nextConfig
+module.exports = nextConfig;
