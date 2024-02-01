535a2441e1169101ff6da944edc930120d9116b4
Lucas Cassimiro <lucascassimiro545@hotmail.com>
2024-02-01 14:13:44 -0300

    caminhos alterados do Button

--- src/app/(home)/shop/[products]/page.tsx
+++ src/app/(home)/shop/[products]/page.tsx
@@ -6,24 +6,24 @@ import Filter from "../../../../assets/filter-ordered.png";
 
 import CardProducts from "@/components/CardProducts";
 
-import { Checkbox, CheckboxGroup, Spinner } from "@nextui-org/react";
+import { Spinner } from "@nextui-org/react";
 
 import { ProductsData } from "@/interfaces/ProductsData";
 
-import ButtonFilterMobile from "@/components/Client-components/ButtonFilterMobile";
-import SelectCategorie from "@/components/Client-components/ButtonFilter/SelectCategorie";
+import ButtonFilterMobile from "@/components/Buttons/ButtonFilterMobile";
+import SelectCategorie from "@/components/Buttons/SelectCategorie";
 import useFilterProducts from "@/hooks/useFilterProducts";
 
 import { CiSearch } from "react-icons/ci";
 import BreadcrumbsShop from "@/components/BreadcrumbsShop";
 import { useEffect, useState } from "react";
+import ButtonFilter from "@/components/Buttons/ButtonFilter";
 
 async function getProducts(param: string): Promise<ProductsData[]> {
   const response = await fetch(
     `https://smartshop-api-foy4.onrender.com/products/${param}`
   );
-  const data = await response.json();
-  return data;
+  return await response.json();
 }
 
 export default function Products({ params }: { params: { products: string } }) {
@@ -77,12 +77,17 @@ export default function Products({ params }: { params: { products: string } }) {
               )}
             </strong>
             <span className="text-sm text-[#7c7b7b] tabletgrande:hidden tablet:block">
-              {/* {product.length} resultados */}
+              {product.length} resultados
             </span>
           </div>
 
           <div className="hidden tablet:block">
-            <ButtonFilterMobile />
+            <ButtonFilterMobile
+              categorie={categorie}
+              handleChangePrice={handleChangePrice}
+              handleChangeStorage={handleChangeStorage}
+              handleChangeColor={handleChangeColor}
+            />
           </div>
         </div>
         <div className="flex">
@@ -98,177 +103,22 @@ export default function Products({ params }: { params: { products: string } }) {
               <Image src={Filter} alt="Ícone filtrar" />
               <h3 className="font-semibold text-lg">Filtrar por:</h3>
             </div>
-            <CheckboxGroup className="flex flex-col w-[12rem]" color="primary">
-              <h4 className="font-bold mb-3">Preço</h4>
-              <label className="cursor-pointer">
-                <Checkbox
-                  type="checkbox"
-                  name="preco"
-                  value="0-1000"
-                  onChange={handleChangePrice}
-                />
-                R$ 0 - R$1000
-              </label>
-              <label className="cursor-pointer">
-                <Checkbox
-                  type="checkbox"
-                  name="preco"
-                  value="1001 - 2000"
-                  onChange={handleChangePrice}
-                />
-                R$ 1.001 - R$ 2.000
-              </label>
-              <label className="cursor-pointer">
-                <Checkbox
-                  type="checkbox"
-                  name="preco"
-                  value="2001 - 3000"
-                  onChange={handleChangePrice}
-                />
-                R$ 2.001 - R$ 3.000
-              </label>
-              <label className="cursor-pointer">
-                <Checkbox
-                  type="checkbox"
-                  name="preco"
-                  value="3001-4000"
-                  onChange={handleChangePrice}
-                />
-                R$ 3.001 - R$ 4.000
-              </label>
-              <label className="cursor-pointer">
-                <Checkbox
-                  type="checkbox"
-                  name="preco"
-                  value="4001-5000"
-                  onChange={handleChangePrice}
-                />
-                R$ 4.001 - R$ 5.000
-              </label>
-              <label className="cursor-pointer">
-                <Checkbox
-                  type="checkbox"
-                  name="preco"
-                  value="5001-6000"
-                  onChange={handleChangePrice}
-                />
-                R$ 5.001 - R$ 6.000
-              </label>
-              <label className="cursor-pointer">
-                <Checkbox
-                  type="checkbox"
-                  name="preco"
-                  value="6001-7000"
-                  onChange={handleChangePrice}
-                  className="mb-3"
-                />
-                R$ 6.001 - R$ 7.000
-              </label>
-              {categorie !== "smartwatch" && categorie !== "fone" && (
-                <>
-                  <h4 className="font-bold mb-3">Armazenamento</h4>
-                  <label className="cursor-pointer">
-                    <Checkbox
-                      type="checkbox"
-                      name="armazenamento"
-                      value="64"
-                      onChange={handleChangeStorage}
-                    />
-                    64 GB
-                  </label>
-                  <label className="cursor-pointer">
-                    <Checkbox
-                      type="checkbox"
-                      name="armazenamento"
-                      value="128"
-                      onChange={handleChangeStorage}
-                    />
-                    128 GB
-                  </label>
-                  <label className="cursor-pointer">
-                    <Checkbox
-                      type="checkbox"
-                      name="armazenamento"
-                      value="256"
-                      onChange={handleChangeStorage}
-                    />
-                    256 GB
-                  </label>
-                  <label className="cursor-pointer">
-                    <Checkbox
-                      type="checkbox"
-                      name="armazenamento"
-                      value="512"
-                      onChange={handleChangeStorage}
-                    />
-                    512 GB
-                  </label>
-                  <label className="cursor-pointer">
-                    <Checkbox
-                      type="checkbox"
-                      name="armazenamento"
-                      value="1024"
-                      onChange={handleChangeStorage}
-                      className="mb-3"
-                    />
-                    1 TB
-                  </label>
-                </>
-              )}
-
-              <h4 className="font-bold mb-3">Cor</h4>
-              <label className="cursor-pointer">
-                <Checkbox
-                  type="checkbox"
-                  name="cor"
-                  value="azul"
-                  onChange={handleChangeColor}
-                />
-                Azul
-              </label>
-              <label className="cursor-pointer">
-                <Checkbox
-                  type="checkbox"
-                  name="cor"
-                  value="branco"
-                  onChange={handleChangeColor}
-                />
-                Branco
-              </label>
-              <label className="cursor-pointer">
-                <Checkbox
-                  type="checkbox"
-                  name="cor"
-                  value="preto"
-                  onChange={handleChangeColor}
-                />
-                Preto
-              </label>
-              <label className="cursor-pointer">
-                <Checkbox
-                  type="checkbox"
-                  name="cor"
-                  value="roxo"
-                  onChange={handleChangeColor}
-                />
-                Roxo
-              </label>
-              <label className="cursor-pointer">
-                <Checkbox
-                  type="checkbox"
-                  name="cor"
-                  value="dourado"
-                  onChange={handleChangeColor}
-                  className="mb-5"
-                />
-                Dourado
-              </label>
-            </CheckboxGroup>
+            <ButtonFilter
+              categorie={categorie}
+              handleChangePrice={handleChangePrice}
+              handleChangeStorage={handleChangeStorage}
+              handleChangeColor={handleChangeColor}
+            />
           </div>
 
           <div className="flex flex-col w-full ml-14 tabletgrande:ml-0 tabletgrande:flex-row tablet:flex-col">
             <div className="tablet:hidden">
-              <ButtonFilterMobile />
+              <ButtonFilterMobile
+                categorie={categorie}
+                handleChangePrice={handleChangePrice}
+                handleChangeStorage={handleChangeStorage}
+                handleChangeColor={handleChangeColor}
+              />
             </div>
 
             <div className="h-[2rem] flex items-center gap-3 w-full justify-end mb-20 tabletgrande:hidden">
