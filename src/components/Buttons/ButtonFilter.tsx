7466696588f500addf4ddd511cc9a67fe28cdc14
Lucas Cassimiro <lucascassimiro545@hotmail.com>
2024-02-01 14:17:26 -0300

    caminhos alterados

---
+++ src/components/Buttons/ButtonFilter.tsx
@@ -0,0 +1,187 @@
+"use client";
+
+import { CheckboxGroup, Checkbox } from "@nextui-org/react";
+import { ChangeEvent } from "react";
+
+interface ButtonFilterProps {
+  categorie: string;
+  handleChangePrice: (event: ChangeEvent<HTMLInputElement>) => void;
+  handleChangeStorage: (event: ChangeEvent<HTMLInputElement>) => void;
+  handleChangeColor: (event: ChangeEvent<HTMLInputElement>) => void;
+}
+
+export default function ButtonFilter({
+  categorie,
+  handleChangePrice,
+  handleChangeStorage,
+  handleChangeColor,
+}: ButtonFilterProps) {
+  return (
+    <CheckboxGroup className="flex flex-col w-[12rem]" color="primary">
+      <h4 className="font-bold mb-3">Preço</h4>
+      <label className="cursor-pointer">
+        <Checkbox
+          type="checkbox"
+          name="preco"
+          value="0-1000"
+          onChange={handleChangePrice}
+        />
+        R$ 0 - R$1000
+      </label>
+      <label className="cursor-pointer">
+        <Checkbox
+          type="checkbox"
+          name="preco"
+          value="1001 - 2000"
+          onChange={handleChangePrice}
+        />
+        R$ 1.001 - R$ 2.000
+      </label>
+      <label className="cursor-pointer">
+        <Checkbox
+          type="checkbox"
+          name="preco"
+          value="2001 - 3000"
+          onChange={handleChangePrice}
+        />
+        R$ 2.001 - R$ 3.000
+      </label>
+      <label className="cursor-pointer">
+        <Checkbox
+          type="checkbox"
+          name="preco"
+          value="3001-4000"
+          onChange={handleChangePrice}
+        />
+        R$ 3.001 - R$ 4.000
+      </label>
+      <label className="cursor-pointer">
+        <Checkbox
+          type="checkbox"
+          name="preco"
+          value="4001-5000"
+          onChange={handleChangePrice}
+        />
+        R$ 4.001 - R$ 5.000
+      </label>
+      <label className="cursor-pointer">
+        <Checkbox
+          type="checkbox"
+          name="preco"
+          value="5001-6000"
+          onChange={handleChangePrice}
+        />
+        R$ 5.001 - R$ 6.000
+      </label>
+      <label className="cursor-pointer">
+        <Checkbox
+          type="checkbox"
+          name="preco"
+          value="6001-7000"
+          onChange={handleChangePrice}
+          className="mb-3"
+        />
+        R$ 6.001 - R$ 7.000
+      </label>
+      {categorie !== "smartwatch" && categorie !== "fone" && (
+        <>
+          <h4 className="font-bold mb-3">Armazenamento</h4>
+          <label className="cursor-pointer">
+            <Checkbox
+              type="checkbox"
+              name="armazenamento"
+              value="64"
+              onChange={handleChangeStorage}
+            />
+            64 GB
+          </label>
+          <label className="cursor-pointer">
+            <Checkbox
+              type="checkbox"
+              name="armazenamento"
+              value="128"
+              onChange={handleChangeStorage}
+            />
+            128 GB
+          </label>
+          <label className="cursor-pointer">
+            <Checkbox
+              type="checkbox"
+              name="armazenamento"
+              value="256"
+              onChange={handleChangeStorage}
+            />
+            256 GB
+          </label>
+          <label className="cursor-pointer">
+            <Checkbox
+              type="checkbox"
+              name="armazenamento"
+              value="512"
+              onChange={handleChangeStorage}
+            />
+            512 GB
+          </label>
+          <label className="cursor-pointer">
+            <Checkbox
+              type="checkbox"
+              name="armazenamento"
+              value="1024"
+              onChange={handleChangeStorage}
+              className="mb-3"
+            />
+            1 TB
+          </label>
+        </>
+      )}
+
+      <h4 className="font-bold mb-3">Cor</h4>
+      <label className="cursor-pointer">
+        <Checkbox
+          type="checkbox"
+          name="cor"
+          value="azul"
+          onChange={handleChangeColor}
+        />
+        Azul
+      </label>
+      <label className="cursor-pointer">
+        <Checkbox
+          type="checkbox"
+          name="cor"
+          value="branco"
+          onChange={handleChangeColor}
+        />
+        Branco
+      </label>
+      <label className="cursor-pointer">
+        <Checkbox
+          type="checkbox"
+          name="cor"
+          value="preto"
+          onChange={handleChangeColor}
+        />
+        Preto
+      </label>
+      <label className="cursor-pointer">
+        <Checkbox
+          type="checkbox"
+          name="cor"
+          value="roxo"
+          onChange={handleChangeColor}
+        />
+        Roxo
+      </label>
+      <label className="cursor-pointer">
+        <Checkbox
+          type="checkbox"
+          name="cor"
+          value="dourado"
+          onChange={handleChangeColor}
+          className="mb-5"
+        />
+        Dourado
+      </label>
+    </CheckboxGroup>
+  );
+}
