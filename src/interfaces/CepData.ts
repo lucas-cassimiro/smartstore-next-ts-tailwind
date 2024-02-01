55c80c612a9ad3c6d48d9fbacbd556f32ce3cc92
Lucas Cassimiro <lucascassimiro545@hotmail.com>
2024-02-01 14:19:04 -0300

    cepData colocado no lugar certo

---
+++ src/interfaces/CepData.ts
@@ -0,0 +1,13 @@
+export interface CepData {
+  bairro: string;
+  cep: number;
+  complemento: string;
+  ddd: string;
+  gia: string;
+  ibge: string;
+  localidade: string;
+  logradouro: string;
+  siafi: string;
+  uf: string;
+  price: number;
+}
