525257bc335a08b228c1ede7620530d178ff8e0d
Lucas Cassimiro <lucascassimiro545@hotmail.com>
2024-02-01 14:12:57 -0300

    alterado de "produtos" para "products"

---
+++ src/app/(home)/@modal/(.)products/[id]/page.tsx
@@ -0,0 +1,26 @@
+import ProductFrame from "@/components/ProductFrame";
+import { ProductsData } from "@/interfaces/ProductsData";
+
+import Modal from "@/components/ModalProductsDetail";
+
+async function getProductById(productId: number): Promise<ProductsData[]> {
+  const request = await fetch(
+    `https://smartshop-api-foy4.onrender.com/products/${productId}`
+  );
+  const data = await request.json();
+  return data;
+}
+
+export default async function ProductModal({
+  params,
+}: {
+  params: { id: number };
+}) {
+  const product = await getProductById(params.id);
+
+  return (
+    <Modal>
+      <ProductFrame products={product} />
+    </Modal>
+  );
+}
