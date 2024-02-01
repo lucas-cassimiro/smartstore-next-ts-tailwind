4afaca1fe16e043f98a9a75ea5aaf87d6c7fb9ea
Lucas Cassimiro <lucascassimiro545@hotmail.com>
2024-02-01 14:14:10 -0300

    console log removido

--- src/components/AuthGuardAdmin.tsx
+++ src/components/AuthGuardAdmin.tsx
@@ -10,8 +10,6 @@ export const AuthGuardAdmin = ({ children }: { children: React.ReactNode }) => {
 
   const { isAuthenticated, isLoading, user } = useAuth();
 
-  console.log(user);
-
   useEffect(() => {
     if (!isAuthenticated && !isLoading && !user?.admin_auth) {
       router.push("/");
