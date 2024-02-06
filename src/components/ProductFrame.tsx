19aeffd0388b9f8a7602d50e3244c7a3a72482a8
Lucas Cassimiro <lucascassimiro545@hotmail.com>
2024-02-06 11:56:51 -0300

    frame

--- src/components/ProductFrame.tsx
+++ src/components/ProductFrame.tsx
@@ -15,13 +15,13 @@ export default function ProductFrame({ products }: CardProductsProps) {
     <div className="w-full h-full flex justify-center items-center mt-40 tablet:mt-0">
       {products.map((product) => (
         <section
-          className="flex flex-col bg-white h-[800px] w-[800px] p-6 rounded-md gap-4 tablet:h-[500px] tablet:w-[500px]"
+          className="flex flex-col bg-white h-[800px] w-[800px] p-6 rounded-md gap-4 tabletgrande:w-[500px] tabletgrande:h-[500px] celulargrande:w-[300px] celulargrande:p-4"
           key={product.id}
         >
           <div className="flex justify-around">
-            <div className="tablet:w-[130px]">
+            <div className="tabletgrande:w-[130px] celulargrande:w-[80px]">
               <Image
-                className="w-[13rem] h-[14rem] object-contain tablet:w-[8.5rem] tablet:h-[9rem]"
+                className="w-[13rem] h-[14rem] object-contain tabletgrande:w-[8.5rem] tabletgrande:h-[9rem] celulargrande:w-[5rem] celulargrande:h-[6rem]"
                 src={`https://smartshop-api-foy4.onrender.com/tmp/uploads/${product.image}`}
                 alt="Imagem do produto"
                 quality={80}
@@ -35,28 +35,33 @@ export default function ProductFrame({ products }: CardProductsProps) {
                 </span>
               </div>
             </div>
-            <div className="flex flex-col gap-2">
-              <h1 className="text-2xl font-bold tabletgrande:text-xl tablet:text-base">
+            <div className="flex flex-col gap-2 celulargrande:w-[180px]">
+              <h1 className="text-2xl font-bold tabletgrande:text-xl tablet:text-base celulargrande:text-sm tabletgrande:w-[270px] celulargrande:w-[200px]">
                 {product.name}
               </h1>
+
               <div className="flex gap-2 items-center">
-                {product.black_friday && (
-                  <p className="py-[0.375rem] px-5 bg-[#d93a1e] text-white font-semibold rounded-[4px] tabletgrande:text-xs tabletgrande:py-[0.2rem] tabletgrande:px-2">
-                    -{product.discount}%
-                  </p>
-                )}
-                {product.black_friday && (
-                  <p className="bg-[#313131] text-base rounded-[4px] py-[0.375rem] px-5 text-white font-semibold tabletgrande:text-xs tabletgrande:py-[0.2rem] tabletgrande:px-2 min-w-[87px]">
-                    Black Friday
-                  </p>
-                )}
+                <div className="flex gap-1 celulargrande:block">
+                  {product.black_friday && (
+                    <p className="py-[0.375rem] px-5 bg-[#d93a1e] text-white font-semibold rounded-[4px] tabletgrande:text-xs tabletgrande:py-[0.2rem] tabletgrande:px-2 celulargrande:w-[45px]">
+                      -{product.discount}%
+                    </p>
+                  )}
+                  {product.black_friday && (
+                    <p className="bg-[#313131] text-base rounded-[4px] py-[0.375rem] px-5 text-white font-semibold tabletgrande:text-xs tabletgrande:py-[0.2rem] tabletgrande:px-2 min-w-[87px]">
+                      Black Friday
+                    </p>
+                  )}
+                </div>
                 <HalfRating star={product.average_score} />
               </div>
+
               {product.black_friday && (
                 <p className="text-[0.75rem] line-through tabletgrande:text-[0.6rem]">
                   {currencyFormat(product.price)}
                 </p>
               )}
+
               {product.black_friday && (
                 <section className="flex items-end gap-2">
                   <p className="font-semibold text-2xl tabletgrande:text-xl tablet:text-base">
@@ -84,7 +89,7 @@ export default function ProductFrame({ products }: CardProductsProps) {
 
               {!product.black_friday && (
                 <section className="flex items-end gap-2">
-                  <p className="font-semibold text-2xl gap-2 tabletgrande:text-lg">
+                  <p className="font-semibold text-2xl gap-2 tabletgrande:text-lg celulargrande:text-base">
                     {currencyFormat(product.price)}
                   </p>
 
@@ -99,7 +104,7 @@ export default function ProductFrame({ products }: CardProductsProps) {
               )}
 
               {product.black_friday && (
-                <p className="text-[0.9rem] text-black min-w-[7.6875rem] tablet:text-[0.8rem]">
+                <p className="text-[0.9rem] text-black min-w-[7.6875rem] tabletgrande:text-[0.8rem] tablet:text-[0.8rem]">
                   Ou <strong>12x</strong> de{" "}
                   <strong>
                     {currencyFormat(
@@ -108,7 +113,8 @@ export default function ProductFrame({ products }: CardProductsProps) {
                   </strong>
                 </p>
               )}
-              <span className="bg-[#efefef] py-2 px-3 text-sm tabletgrande:text-xs tablet:text-[11px]">
+
+              <span className="bg-[#efefef] py-2 px-3 text-sm tabletgrande:text-xs tablet:text-[11px] celulargrande:text-[10px] celulargrande:py-1 celulargrande:px-2">
                 Ou em <strong>21x</strong> de{" "}
                 <strong>
                   {currencyFormat(
@@ -117,6 +123,7 @@ export default function ProductFrame({ products }: CardProductsProps) {
                   no cartão Bradesco
                 </strong>
               </span>
+
               <span className="bg-[#efefef] py-2 px-3 text-sm tabletgrande:text-xs tablet:text-[11px]">
                 Ou em <strong>21x</strong> de{" "}
                 <strong>
@@ -126,20 +133,6 @@ export default function ProductFrame({ products }: CardProductsProps) {
                   no cartão Itaú
                 </strong>
               </span>
-              {/* <span className="text-sm">Calcular o prazo de entrega</span>
-              <div className="flex gap-2">
-                <input
-                  placeholder="Insira seu CEP"
-                  className="w-32 border border-[#c0c0c0] text-center"
-                />
-                <button className="p-4 bg-black text-white text-sm rounded-[3px]">
-                  Calcular
-                </button>
-              </div> */}
-              {/* <div className="flex items-center gap-1 cursor-pointer w-[172px]">
-                <FaLocationDot />
-                <span className="text-sm">Use minha localização</span>
-              </div> */}
               <ButtonAddToCart products={product} />
             </div>
           </div>
