b5e28399254b12bd907145832745fea1d2725f6e
Lucas Cassimiro <lucascassimiro545@hotmail.com>
2024-02-06 11:56:46 -0300

    cursor pointer

--- src/components/ModalProductsDetail.tsx
+++ src/components/ModalProductsDetail.tsx
@@ -7,17 +7,17 @@ interface ModalProps {
 }
 
 export default function ModalProductsDetail({ children }: ModalProps) {
-  const { onClick, onDismiss, overlay, wrapper } = useModal();
+  const { onClick, overlay, wrapper } = useModal();
 
   return (
     <div
       ref={overlay}
-      className="fixed z-10 left-0 right-0 top-0 bottom-0 mx-auto bg-black/40 scrollbar overflow-y-auto"
+      className="fixed z-10 left-0 right-0 top-0 bottom-0 mx-auto bg-black/40 scrollbar overflow-y-auto cursor-pointer"
       onClick={onClick}
     >
       <div
         ref={wrapper}
-        className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 p-6 tabletgrande:w-1/2"
+        className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 p-6 tabletgrande:w-1/2 cursor-default"
       >
         {children}
       </div>
